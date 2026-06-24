c5eaca84e0e0aa9f99c1eb59bb91cd43de7c0821
AnizaHelwa <helwamahanani07@gmail.com>
2026-06-25 01:33:55 +0700

    fix: allow portfolio to be embedded on google sites

---
+++ app/Http/Middleware/AllowEmbed.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AllowEmbed
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $next($request);
+        
+        // Menghapus proteksi X-Frame-Options agar bisa dibaca Google Sites
+        $response->headers->set('X-Frame-Options', 'ALLOWALL'); 
+        $response->headers->set('Content-Security-Policy', "frame-ancestors 'self' https://sites.google.com");
+
+        return $response;
+    }
+}
