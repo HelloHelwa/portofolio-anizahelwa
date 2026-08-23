436c6f91396248ead69a81c43b7f51b8ad560755
AnizaHelwa <helwamahanani07@gmail.com>
2026-08-24 01:41:11 +0700

    fix api for vercel

--- api/index.php
+++ api/index.php
@@ -1,12 +1,28 @@
 <?php
 
-// Arahkan storage dan cache ke direktori /tmp milik Vercel
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('log_errors', 1);
+
+$dirs = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache',
+    '/tmp/storage/framework/cache/data',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/logs',
+];
+foreach ($dirs as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0755, true);
+    }
+}
+
 $_ENV['APP_CONFIG_CACHE'] = '/tmp/config.php';
 $_ENV['APP_EVENTS_CACHE'] = '/tmp/events.php';
 $_ENV['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
 $_ENV['APP_ROUTES_CACHE'] = '/tmp/routes.php';
 $_ENV['APP_SERVICES_CACHE'] = '/tmp/services.php';
-$_ENV['VIEW_COMPILED_PATH'] = '/tmp';
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
 $_ENV['CACHE_STORE'] = 'array';
 $_ENV['SESSION_DRIVER'] = 'cookie';
 $_ENV['LOG_CHANNEL'] = 'stderr';
