9ebdbd44932547b78c36d63c939c6e3f11aa8e4d
AnizaHelwa <anizahelwa@student.ub.ac.id>
2026-09-12 00:27:26 +0700

    test Laravel basic route

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,11 @@ Route::get('/', function () {
     return 'Laravel is working!';
 });
 
-Route::get('/pillars', [PortfolioController::class, 'pillars'])->name('portfolio.pillars');
+Route::get('/pillars', [PortfolioController::class, 'pillars'])
+    ->name('portfolio.pillars');
 
-Route::get('/missions', [PortfolioController::class, 'missions'])->name('portfolio.missions');
+Route::get('/missions', [PortfolioController::class, 'missions'])
+    ->name('portfolio.missions');
 
 Route::get('/projects', function () {
     return view('projects');
