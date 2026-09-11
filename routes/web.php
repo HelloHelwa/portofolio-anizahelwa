16185ca1257eb2cdbf9e4ebcb2379de391668c0c
AnizaHelwa <anizahelwa@student.ub.ac.id>
2026-09-12 00:22:26 +0700

    test Laravel basic route

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,14 @@
 use App\Http\Controllers\PortfolioController;
 use Illuminate\Support\Facades\Route;
 
-// Mengarahkan halaman utama (/) langsung ke PortfolioController
-Route::get('/', [PortfolioController::class, 'index'])->name('portfolio.index');
+Route::get('/', function () {
+    return 'Laravel is working!';
+});
 
 Route::get('/pillars', [PortfolioController::class, 'pillars'])->name('portfolio.pillars');
+
 Route::get('/missions', [PortfolioController::class, 'missions'])->name('portfolio.missions');
+
 Route::get('/projects', function () {
     return view('projects');
 });
